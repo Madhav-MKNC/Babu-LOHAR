1c48c240152123a57985b0007159b5be2ba994b0
Madhav Kumar <78339236+Madhav-MKNC@users.noreply.github.com>
2023-07-01 06:36:44 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+# ##### main.py #####
+
+from os import system as cmd 
+cmd("pip install -r requirements.txt")
+
+from bot import start_bot_server
+from web import keep_alive
+
+keep_alive()
+start_bot_server()
