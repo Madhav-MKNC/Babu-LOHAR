e392f2a6fd02b8bd2b7520c31801031391890578
Madhav Kumar <78339236+Madhav-MKNC@users.noreply.github.com>
2023-07-07 10:09:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 ##### main.py #####
 
-from os import system as cmd
+def main():
+  from bot import start_bot_server
+  from web import keep_alive
 
-cmd("pip install -r requirements.txt")
+  keep_alive()
+  start_bot_server()
 
-from bot import start_bot_server
-from web import keep_alive
-
-keep_alive()
-start_bot_server()
+try:
+  main()
+except ImportError:
+  from os import system as cmd
+  cmd("pip install -r requirements.txt")
+  main()
